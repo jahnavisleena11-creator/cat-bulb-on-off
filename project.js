26a8751889670940f109fca7f327dffef314e3d2
jahnavi sleena <jahnavisleena11@gmail.com>
2026-07-31 21:58:06 +0530

    Fixed image paths

--- project.js
+++ project.js
@@ -1,16 +1,16 @@
 
 function on() {
-    document.getElementById("bulb").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
-    document.getElementById("cat").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("bulb").src = "images/bulb-on.png";
+    document.getElementById("cat").src = "images/cat.png";
     document.getElementById("textt").textContent = "Switched on";
-    document.getElementById("butto").style.backgroundColor = " #cbd2d9";
-    document.getElementById("butt").style.backgroundColor = " red";
+    document.getElementById("butto").style.backgroundColor = "#cbd2d9";
+    document.getElementById("butt").style.backgroundColor = "red";
 }
 
 function off() {
-    document.getElementById("bulb").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
-    document.getElementById("cat").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("bulb").src = "images/bulb-off.png";
+    document.getElementById("cat").src = "images/cat-eyes.png";
     document.getElementById("textt").textContent = "Switched off";
-    document.getElementById("butt").style.backgroundColor = " #cbd2d9";
+    document.getElementById("butt").style.backgroundColor = "#cbd2d9";
     document.getElementById("butto").style.backgroundColor = "green";
 }
